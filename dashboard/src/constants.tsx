a5e32425bb0a2e29dd2e8cdf2a76a619a3aed715
gmatosferreira <goncaloafmatos@tecnico.ulisboa.pt>
2026-04-15 14:56:10 +0100

    #12 data url considering always remote data

--- dashboard/src/constants.tsx
+++ dashboard/src/constants.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { MetricDef, ViewLevel, ScaleMethod } from './types';
 
-const DATA_BASE_URL = import.meta.env.DEV ? 'data/' : 'https://ushift.tecnico.ulisboa.pt/content/impt/';
+const DATA_BASE_URL = 'https://ushift.tecnico.ulisboa.pt/content/impt/';
 
 export const LEVEL_CONFIG: Record<ViewLevel, { file: string, parent?: ViewLevel }> = {
     'municipality': { file: `${DATA_BASE_URL}municipios_aggregated.geojson` },
